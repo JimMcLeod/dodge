e431285601ea98df024d692120e2a3ea5bff4646
JimMcLeod <jimcleod@gmail.com>
2015-07-27 17:05:37 +0100

    add sprite to group

--- script/mainloop.js
+++ script/mainloop.js
@@ -7,49 +7,9 @@ window.onload = function() {
 	var background,
 		logo;
 
-	var Menu = {
-		preload: function () {
-			this.load.image('background', 'img/background.png');
-			this.load.image('logo', 'img/logo.png');
-		},
-
-		create: function () {
-			this.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
-            this.scale.refresh();
-
-			background = this.add.image(this.world.centerX, this.world.centerY, 'background');
-			background.anchor.setTo(0.5, 0.5);
-
-			background.inputEnabled = true;
-			game.input.onDown.add(Menu.tapped, this);
-
-			logo = this.add.sprite(this.world.centerX, 140, 'logo');
-			logo.anchor.setTo(0.5, 0.5);
-
-			var style = {font: "20px Arial", fill: "#ffffff", align: "center"};
-			var text = this.add.text(this.world.centerX, 400, "TAP TO START", style);
-			text.anchor.setTo(0.5, 0.5);
-
-		},
-
-		update: function () {
-
-		},
-
-		tapped: function () {
-			game.state.start('Game');
-		}
-	};
-
-
 	var Game = {
 		player: null,
 		ball: null,
-
-		preload: function () {
-			this.load.image('player', 'img/player.png');
-			this.load.image('ball', 'img/ball.png');
-		},
 
 		create: function () {
 			this.physics.startSystem(Phaser.Physics.ARCADE);
@@ -70,6 +30,13 @@ window.onload = function() {
 			}
 			ballGroup.setAll('anchor.x', 0.5);
             ballGroup.setAll('anchor.y', 0.5);
+
+            var _this = this;
+			setInterval(function () {
+				var ball = _this.add.sprite(_this.world.randomX, _this.world.randomY, 'ball');
+				ball.anchor.setTo(0.5, 0.5);
+				ballGroup.add(ball);
+			}, 1000);
 		},
 
 		update: function () {
@@ -81,11 +48,57 @@ window.onload = function() {
 					ball.destroy();
 				}
             }, this);
+
+
+		}
+	};
+
+	var Menu = {
+		create: function () {
+			this.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
+			this.scale.refresh();
+
+			background = this.add.image(this.world.centerX, this.world.centerY, 'background');
+			background.anchor.setTo(0.5, 0.5);
+
+			background.inputEnabled = true;
+			game.input.onDown.add(Menu.tapped, this);
+
+			logo = this.add.sprite(this.world.centerX, 140, 'logo');
+			logo.anchor.setTo(0.5, 0.5);
+
+			var style = {font: "20px Arial", fill: "#ffffff", align: "center"};
+			var text = this.add.text(this.world.centerX, 400, "TAP TO START", style);
+			text.anchor.setTo(0.5, 0.5);
+		},
+
+		tapped: function () {
+			this.state.start('Game');
+		}
+	};
+
+	var Boot = {
+		preload: function () {
+			this.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
+			this.scale.refresh();
+
+			this.load.image('background', 'img/background.png');
+			this.load.image('logo', 'img/logo.png');
+			this.load.image('player', 'img/player.png');
+			this.load.image('ball', 'img/ball.png');
+		},
+
+		create: function () {
+			this.input.maxPointers = 1;
+			this.stage.disableVisibilityChange = true;
+
+			this.state.start('Menu');
 		}
 	};
 
 	game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, '');
 	game.state.add('Game', Game);
 	game.state.add('Menu', Menu);
-	game.state.start('Menu');
+	game.state.add('Boot', Boot);
+	game.state.start('Boot');
 };
